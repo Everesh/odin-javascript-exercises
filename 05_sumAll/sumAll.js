405aafb65adde744c89b4d256d009de4cf46a07b
Everesh <everesh@proton.me>
2024-04-23 14:55:08 +0200

    Finish 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if ( typeof(num1) != typeof(num2) || typeof(num1) != 'number'){
+        return "ERROR";
+    }
+    else if (num1 > num2) {
+        let tmp = num1;
+        num1 = num2;
+        num2 = tmp;
+    }
 
+    if (num1 < 0) {
+        return "ERROR";
+    }
+    
+    let sum = 0;
+    while (num1 <= num2) {
+        sum += num1;
+        num1++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
