4b2d3592f4e6b9e502db295ecea45ddf095ec320
Everesh <everesh@proton.me>
2024-04-23 03:03:37 +0200

    Finish 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverse = [];
+    for (const letter of string) {
+        reverse.unshift(letter);
+    }
+    return reverse.join("");
 };
 
 // Do not edit below this line
