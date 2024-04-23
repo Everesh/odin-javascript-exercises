e7e1049f1055b070e53c9cef66af4ba3f6373c82
Everesh <everesh@proton.me>
2024-04-23 03:12:35 +0200

    Finish 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...exclude) {
+    let output = [];
+    for (const position of arr) {
+        if (!exclude.includes(position)) {
+            output.push(position);            
+        }
+    }
+    return output;
 };
 
 // Do not edit below this line
