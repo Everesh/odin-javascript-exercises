022fe7921c5d44d9cd56a1f3c625be0965699950
Everesh <everesh@proton.me>
2024-04-23 02:57:58 +0200

    Finish 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    let output = "";
+    for (let x = 0; x < num; x++){
+        output += string;
+    }
+    return output;
 };
 
 // Do not edit below this line
