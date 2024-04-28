b131844857597565d9f4c6be0491cf945e998c79
Everesh <everesh@proton.me>
2024-04-28 20:30:30 +0200

    Finish 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num == 0){
+        return 0;
+    }
+    else if (num < 0 || isNaN(num)){
+        return "OOPS"
+    }
+    let tailing = 0;
+    let leading = 1;
+    for(let x = 1; x < num; x++){
+        let tmp = tailing;
+        tailing = leading;
+        leading = tmp + leading;
+    }
+    return leading;
 };
 
 // Do not edit below this line
