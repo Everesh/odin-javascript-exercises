e47dc207c35972a184cf522ba7a93e5af96ed6db
Everesh <everesh@proton.me>
2024-04-28 20:22:13 +0200

    Finish 09_palidromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const sanitized = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    if (sanitized.length % 2 == 0) {
+        sanitized.split('').splice(sanitized.lenght/2, 1).join('');
+    }
+    const firstHalf = sanitized.slice(0,sanitized.lenght);
+    const secondHalf = sanitized.slice(sanitized.lenght);
+    if (firstHalf == secondHalf.split('').reverse().join('')) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
