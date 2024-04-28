de3cb6f6def4a9936bc8e7c53a198ae91aa7a4eb
Everesh <everesh@proton.me>
2024-04-28 20:50:50 +0200

    finish 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
 
+      return arr.slice()
+                .map(item => {
+                    if (!("yearOfDeath" in item)) {
+                        item.yearOfDeath = (new Date()).getFullYear();
+                    }
+                    return item;
+                })
+                .sort( (a,b) => (b.yearOfDeath-b.yearOfBirth) - (a.yearOfDeath-a.yearOfBirth))[0];
 };
 
 // Do not edit below this line
